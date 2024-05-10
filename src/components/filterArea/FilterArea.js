b82016745cd5cf733cb8f7dde4196f287f071cf8
AlexeyLush <alexeylush4060@gmail.com>
2024-05-10 17:43:25 +0300

    feat(FilterArea): добавлен компонент для филтрации

---
+++ src/components/filterArea/FilterArea.js
@@ -0,0 +1,35 @@
+import "./FilterArea.css"
+
+function FilterArea() {
+
+    return (
+        <div className="filter-area block__padding_horizontal_big block__padding_vertical_big block__margin_top_small">
+            <div className="filter-area__header block__padding_horizontal_big block__padding_vertical_big">
+                <div className="placeholder_image" style={{ backgroundImage: `url('/images/pictures/placeholder_image.png')`}}>
+
+                </div>
+                <div className="header__description block__margin_top_big block__margin_bottom_big">
+                    <p>
+                        В нашей жизни, когда время становится все более ценным ресурсом, задача планирования приема пищи
+                        становится все более сложной.
+                    </p>
+                    <p>
+                        Часто мы сталкиваемся с дилеммой: что приготовить на завтрак, обед или ужин? Каким образом мы
+                        можем легко и быстро определиться с выбором блюда и не тратить много времени на принятие этого
+                        решения?
+                    </p>
+                    <p>
+                        Наш сервис поможет: выбирайте параметры - и вперед!
+                    </p>
+                </div>
+            </div>
+            <div className="filter-area__input_form block__margin_bottom_big">
+
+            </div>
+        </div>
+    );
+
+
+}
+
+export default FilterArea;
