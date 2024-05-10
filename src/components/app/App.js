33081d188a4925ca74be2e7c793ca95113d4c15c
AlexeyLush <alexeylush4060@gmail.com>
2024-05-10 16:39:22 +0300

    refactor(App): добавил Header и основу для роутинга

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,27 @@
-import logo from '../../logo.svg';
 import './App.css';
+import {Routes} from "react-router-dom";
+import Header from "../header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="block__padding_horizontal block__padding_vertical">
+        <Header title={"Сборник рецептов из разных стран мира"} isHomeReturn={true}/>
+        <Routes>
+          {/*<Route exact path="/" element={<Home/>}/>*/}
+          {/*/!*<Route path="/products" element={<Products/>}/>*!/*/}
+          {/*<Route path="/catalog/:product_type/:id" element={<ProductPage/>}/>*/}
+          {/*<Route path="/catalog/:product_type" element={<Products/>}/>*/}
+          {/*<Route path="/catalog/chairs/:id" element={<ChairPage/>}/>*/}
+
+          {/*<Route path="/cart" element={<Cart/>}/>*/}
+          {/*<Route path="/contacts" element={<Contacts/>}/>*/}
+          {/*<Route path="/delivery-and-payment" element={<DeliveryAndPayment/>}/>*/}
+          {/*<Route path="/order" element={<Order/>}/>*/}
+          {/*<Route path="*" element={<NotFound/>}/>*/}
+        </Routes>
+      </div>
+    </>
   );
 }
 
