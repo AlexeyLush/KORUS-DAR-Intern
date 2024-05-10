20da6fc4ea7fb3e7e2ef6d63550657af41c05201
AlexeyLush <alexeylush4060@gmail.com>
2024-05-10 16:39:47 +0300

    refactor(ArrowButton): добавил кнопку со стрелкой

---
+++ src/components/arrowButton/ArrowButton.js
@@ -0,0 +1,12 @@
+import './ArrowButton.css'
+import leftArrow from "../../images/svg/left_arrow.svg"
+
+function ArrowButton() {
+
+    return (
+        <img className="arrow" src={leftArrow}/>
+    )
+
+}
+
+export default ArrowButton;
