711142cc66a3cc24bdef7ac2b18abec0304dbdb7
AlexeyLush <alexeylush4060@gmail.com>
2024-05-10 16:40:00 +0300

    refactor(Header): добавил Header

---
+++ src/components/header/Header.js
@@ -0,0 +1,30 @@
+import "./Header.css"
+import ArrowButton from "../arrowButton/ArrowButton";
+import classNames from "classnames";
+
+function Header({title, isHomeReturn}) {
+
+    function processTitle() {
+        return title !== undefined ? title : ""
+    }
+
+    function processIsHomeReturn() {
+        return typeof isHomeReturn === "boolean" ? isHomeReturn : false;
+    }
+
+    let headerTitleClasses = classNames(
+        'header__title',
+        {
+            'title__margin_left': isHomeReturn
+        }
+    );
+
+    return (
+        <header className="header header__light">
+            {processIsHomeReturn() && <ArrowButton/>}
+            <h1 className={headerTitleClasses}>{processTitle()}</h1>
+        </header>
+    );
+}
+
+export default Header;
