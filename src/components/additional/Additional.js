b0b56a4897024a83c94b3b0797f7f6ceefd48ee6
AlexeyLush <alexeylush4060@gmail.com>
2024-05-12 02:09:16 +0300

    feat(Additional): добавил дополнительный блок с кнопкой "Мне повезет!"

---
+++ src/components/additional/Additional.js
@@ -0,0 +1,14 @@
+import "./Additional.css"
+
+function Additional() {
+
+    return (
+        <div className="additional block__margin_top_large">
+            <p className="additional__title">А еще можно попробовать на вкус удачу</p>
+            <input className="additional__button" type="button" value="Мне повезёт!"/>
+        </div>
+    );
+
+}
+
+export default Additional;
