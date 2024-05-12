2fbdecd60b6e7c1ed94ce5abac37834e3eedb794
AlexeyLush <alexeylush4060@gmail.com>
2024-05-12 05:14:11 +0300

    feat(Home): на страницу добавлен список рецептов

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import './Home.css'
 import FilterArea from "../../components/filterArea/FilterArea";
+import ListArea from "../../components/listArea/ListArea";
 
 function Home() {
 
     return (
-        <>
+        <div className="home">
             <FilterArea/>
-        </>
+            <ListArea/>
+        </div>
     );
 
 
